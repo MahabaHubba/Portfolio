facc4480e13acef1e4c69386a08a29fbbe75ef51
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-07-28 22:07:21 +1000

    Start
    Added Welcome to the page, Need to center and enlarge it and need to make the text come in one by one

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="aboutme">
+    <div class="container mt-5">
+        <div class="row justify-content-between">
+                <div class="boxborder bg-light border mb-4">
+                    <h5 class="text-center"> Welcome </h5> 
+                </div>   
+        </div>
     </div>
+</div>
   );
 }
 
