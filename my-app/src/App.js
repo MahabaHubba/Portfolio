669ae293d633d6a43ec852fbfaddb864bf7c23ad
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-07-29 23:17:39 +1000

    Add colour gradient
    added grey to white color gradient

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//Currently is able to come one by one in terms of letters however there is some error
+// import React, { useState, useEffect } from 'react';
+
+// const TextReveal = () => {
+//   const [displayedText, setDisplayedText] = useState('');
+//   const fullText = 'Welcome';
+
+//   useEffect(() => {
+//     let index = 0;
+//     const interval = setInterval(() => {
+//       if (index < fullText.length) {
+//         setDisplayedText((prev) => prev + fullText[index]);
+//         index += 1;
+//       } else {
+//         clearInterval(interval);
+//       }
+//     }, 100);
+//     return () => clearInterval(interval);
+//   }, [fullText]);
+
+//   return <span>{displayedText}</span>;
+// };
 
 function App() {
   return (
-    <div class="aboutme">
-    <div class="container mt-5">
-        <div class="row justify-content-between">
-                <div class="boxborder bg-light border mb-4">
-                    <h5 class="text-center"> Welcome </h5> 
-                </div>   
+    <div className="gradient-bg">
+      <div className="container mt-5">
+        <div className="row justify-content-between">
+          <div className="boxborder bg-light border mb-4">
+            <h5 className="text-center"> Welcome </h5> 
+          </div>   
         </div>
+      </div>
     </div>
-</div>
   );
 }
 
