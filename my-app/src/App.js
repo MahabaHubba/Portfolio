0e674961b53b25c577a288253ec54402bad3f6bf
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-08-03 22:09:00 +1000

    Addition
    Add Navbar to the scheme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 
 //Currently is able to come one by one in terms of letters however there is some error
@@ -32,24 +32,41 @@ function App() {
     <div className="container gradient-bg">
    
     <div className="row ">
-      
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">MySite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     <div className="col-12 welcome">
-    <h5 className="text-center welcometext"> Welcome </h5> 
+    <h5 className="text-center welcometext bouncing-text"> Welcome </h5> 
     </div>
 
     <div class="row">
   <div class="col-md-4 aboutme1">About Me</div>
-  <div class="col-md-8">Little segements</div>
+  <div class="col-md-8 aboutmesegment">Little segements</div>
 </div>
 
     <div class="row">
-  <div class="col-md-8 portfolio">Portfolio</div>
-  <div class="col-md-4">Dashes</div>
+  <div class="col-md-8 portfolio">Dashes</div>
+  <div class="col-md-4 portfolio">Portfolio</div>
 </div>
 
 <div class="row">
-  <div class="col-md-4 contact1">Conact Me</div>
-  <div class="col-md-8">Icons</div>
+  <div class="col-md-4 contact1">Contact Me</div>
+  <div class="col-md-8 contacticon">Icons</div>
 </div>
 
   </div>
