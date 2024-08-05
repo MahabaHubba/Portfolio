801c452db49b56683414cd79f87e3f2f0a4ffc44
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-08-05 22:33:16 +1000

    We nearely getting there
    Added pokemon pixel font and ability to scroll through the desired page when clicked

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav} from 'react-bootstrap';
+import { useRef } from 'react';
+
 
 
 function App() {
+  const aboutRef = useRef(null);
+  const pastWorkRef = useRef(null);
+  const hobbiesRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToRef = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behaviour: 'smooth'
+    });
+  };
+
+
   return (
     //Overall Container
     <div className="container gradient-bg">
@@ -14,43 +29,42 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className='nav-link-bounce'>About Me</Nav.Link>
-            <Nav.Link className='nav-link-bounce'>Past Work</Nav.Link>
-            <Nav.Link className='nav-link-bounce'>Contact Me</Nav.Link>
+            <Nav.Link className='nav-link-bounce' onClick={() => scrollToRef(aboutRef)}>About Me</Nav.Link>
+            <Nav.Link className='nav-link-bounce' onClick={() => scrollToRef(pastWorkRef)}>Past Work</Nav.Link>
+            <Nav.Link className='nav-link-bounce' onClick={() => scrollToRef(hobbiesRef)}>Hobbies</Nav.Link>
+            <Nav.Link className='nav-link-bounce' onClick={() => scrollToRef(contactRef)}>Contact Me</Nav.Link>
            
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     <div className="col-12 welcome">
-    <h5 className="text-center welcometext bouncing-text"> Welcome </h5> 
+      <p className='pokemonpixel'>N</p>
+    <h5 className="text-center welcometext bouncing-text"> Welcome </h5>
+    <p className='pokemonpixel2'>O</p>
     </div>
 
-    <div class="row">
+    <div class="row" ref={aboutRef}>
   <div class="col-md-4 aboutme1">About Me</div>
   <div class="col-md-8 aboutmesegment">Little segements</div>
 </div>
 
-    <div class="row">
+    <div class="row" ref={pastWorkRef}>
   <div class="col-md-8 portfolio">Dashes</div>
   <div class="col-md-4 portfolio">Portfolio</div>
 </div>
 
-<div class="row">
+<div class="row" ref={hobbiesRef}>
+  <div class="col-md-4 portfolio">Powerlifting</div>
+  <div class="col-md-8 portfolio">Hobbies</div>
+</div>
+
+<div class="row" ref={contactRef}>
   <div class="col-md-4 contact1">Contact Me</div>
   <div class="col-md-8 contacticon">Icons</div>
 </div>
 
   </div>
 </div>
-    // <div className="gradient-bg">
-    //   <div className="container mt-5">
-    //     <div className="row justify-content-between">
-    //       <div className="boxborder bg-light border mb-4">
-    //         <h5 className="text-center"> Welcome </h5> 
-    //       </div>   
-    //     </div>
-    //   </div>
-    // </div>
   );
 
   
