11c8e2eeaa9baaadf7d34c3ac2cda57cd0ec468d
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-08-16 20:25:24 +1000

    Remove
    Remove

--- my-app/src/Sections/contact.jsx
+++ my-app/src/Sections/contact.jsx
@@ -27,9 +27,6 @@ export default function Footer() {
             <a href="https://www.linkedin.com/in/mohibul-haque-14782020b/" target="_blank"  title="LinkedIn" rel="noreferrer">
               <img src="linkedin.png" alt="linkedin logo" />
             </a>
-            <a href="./public/Md-Mohibul-Haque-Resume.pdf" target="_blank"  title="Resume" rel="noreferrer">
-              <img src="resume.png" alt="Resume logo" />
-            </a>
           </div>
     
           {showEmail && (
